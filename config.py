a3cb3654e9f3f61a8e7a9b4806e6ad0582bda13c
Viola Hencel Rodrigues <violahencel.rodrigues@thoughtfocus.com>
2025-06-06 12:55:19 +0530

    Initial push to travel_app branch

---
+++ config.py
@@ -0,0 +1,13 @@
+# File: config.py
+DB_CONNECTION = "Driver={SQL Server};Server=WLI223X8;Trusted_Connection=yes;"# Configuration File
+
+# Database Connection Settings
+DB_CONNECTION = "Driver={SQL Server};Server=WLI223X8;Database=TravelDB;Trusted_Connection=yes;"
+
+
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'violahencel2002@gmail.com'
+MAIL_PASSWORD = 'rogc peyn utgr oeww'
